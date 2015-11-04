844d76f9a68d993a62465b5beb08a3682d8179c3
rich1210 <07richardfrancis@gmail.com>
2015-11-04 14:01:45 -0800

    Snow and moving paint

---
+++ Assets/respawnPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawnPoint : MonoBehaviour
+{
+    // variables
+    public GameObject player;
+    public Vector3 pos; 
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        // assign reference
+        player = GameObject.Find("FPSController");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    // 
+        if(player.transform.position.y < -100f)
+        {
+            // you have fallen and died
+            player.transform.position = pos;
+        }
+	}
+}
