b29384c27b7698c5246ed7d5a207ed0c170bc5ef
pixelgriffin <navarrogriffin@gmail.com>
2015-11-17 21:44:07 -0800

    Finished game

---
+++ Assets/Standard Assets/Scripts/EscapeGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeGame : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+}
