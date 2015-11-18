789a26379999dfdfde4549e60a9b6546010545f2
pixelgriffin <navarrogriffin@gmail.com>
2015-11-17 22:29:07 -0800

    Added music and a few bug fixes

---
+++ Assets/Standard Assets/Scripts/LoopingMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopingMusic : MonoBehaviour {
+
+	private static LoopingMusic inst = null;
+
+	public static LoopingMusic Instance {
+		get { return inst; }
+	}
+
+	void Awake() {
+		if(inst != null && inst != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			inst = this;
+		}
+
+		this.GetComponent<AudioSource>().loop = true;
+		this.GetComponent<AudioSource>().Play();
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
